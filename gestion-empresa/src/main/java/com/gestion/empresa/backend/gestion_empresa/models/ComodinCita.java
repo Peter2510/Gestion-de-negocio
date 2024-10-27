07605dceaf99485e6777978afeb6976877b0ddbc
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-10-27 12:56:31 -0600

    Cambios a comodin cita

--- gestion-empresa/src/main/java/com/gestion/empresa/backend/gestion_empresa/models/ComodinCita.java
+++ gestion-empresa/src/main/java/com/gestion/empresa/backend/gestion_empresa/models/ComodinCita.java
@@ -1,15 +1,8 @@
 package com.gestion.empresa.backend.gestion_empresa.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Author: alexxus
  * Created on: 24/10/24
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ComodinCita {
 }
